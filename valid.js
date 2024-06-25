6669635689556df4dd10f28d4066b1fd4bf10d42
WASABYK <119607289+WASABYK@users.noreply.github.com>
2024-06-26 02:03:21 +0300

    Update valid.js

--- valid.js
+++ valid.js
@@ -94,7 +94,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         // Сохраняем информацию о проверке почты
                         localStorage.setItem('emailVerified', 'true');
                         history.replaceState(null, '', 'success.html');
-                        window.location.href = 'index.htm'; // Укажите здесь вашу страницу для перехода
+                        window.location.href = 'register.htm'; // Укажите здесь вашу страницу для перехода
                     }
                 });
             } else {
